6a84610459bbe7b1d19e10401e057ef1e6c2d679
thirumurugan7 <thiru82003@gmail.com>
2022-11-20 21:20:02 +0530

    configured cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+// let allowed = ["http://localhost:3000", "some otherlinks"];
+// function options(req, res) {
+//   let tmp;
+//   let origin = req.header("Origin");
+//   if (allowed.indexOf(origin) > -1) {
+//     tmp = {
+//       origin: true,
+//       optionSuccessStatus: 200,
+//     };
+//   } else {
+//     tmp = {
+//       origin: "stupid",
+//     };
+//   }
+//   res(null, tmp);
+// }
+// app.use(cors(options)); //specify the origin --> from where the server can be accessed usually frontend
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("welcome to home");
 });
@@ -9,7 +27,7 @@ app.get("/friends", (req, res) => {
   res.send("welcome to friends");
 });
 app.get("/books", (req, res) => {
-  res.send("welcome to books");
+  res.send("welcome to harrypotter");
 });
 
 app.listen(8000, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,21 @@
 function App() {
+  const get = async () => {
+    const res = await fetch("http://localhost:8000/");
+    console.log(res);
+
+    // console.log("body" + res.body);
+    // console.log(res.bodyUsed);
+    // console.log(res.ok);
+    // console.log(res.json);
+    // console.log(res.type);
+    // console.log(res.url);
+    // console.log(res.statusText);
+    // console.log(res.formData);
+    // console.log(res.headers);
+    // console.log(res.redirected);
+    // console.log(res.blob);
+  };
+  get();
   return <div>welcome to frontend</div>;
 }
 
