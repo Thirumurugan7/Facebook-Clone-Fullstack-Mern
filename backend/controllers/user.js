ab7507bc081e5283bcc1fbef68393ad7990b7ef6
thirumurugan7 <thiru82003@gmail.com>
2022-11-23 15:50:28 +0530

    stored first set of registration details in mongodb

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,33 @@
-exports.home = (req, res) => {
-  //   res.send("Welcome from user home!");
-  res.status(200).json({
-    message: "Welcome from user home",
-    error: "nothing wrong",
-  });
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+
+    const user = await new User({
+      first_name,
+      last_name,
+      email,
+      password,
+      username,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const { home } = require("../controllers/user");
+const { register } = require("../controllers/user");
 
 const router = express.Router();
 
-router.get("/user", home);
-router.get("/friend", (req, res) => {
-  res.send("welcome from friend");
-});
-router.get("/mia", (req, res) => {
-  res.send("mia wants to fuck you");
-});
+router.post("/register", register);
 
 module.exports = router;
