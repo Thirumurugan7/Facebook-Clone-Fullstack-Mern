d05b1da7d6b233dcbe3abdfd4f51f18ec2975686
thirumurugan7 <thiru82003@gmail.com>
2023-01-13 19:34:26 +0530

    redux initiated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000/");
-    console.log(res);
-
-    // console.log("body" + res.body);
-    // console.log(res.bodyUsed);
-    // console.log(res.ok);
-    // console.log(res.json);
-    // console.log(res.type);
-    // console.log(res.url);
-    // console.log(res.statusText);
-    // console.log(res.formData);
-    // console.log(res.headers);
-    // console.log(res.redirected);
-    // console.log(res.blob);
-  };
-  get();
-  return <div>welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
