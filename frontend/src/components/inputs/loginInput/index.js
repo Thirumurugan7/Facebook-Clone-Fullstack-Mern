da4e49c9ebf3df52b713923fc6384eca27a407b0
thirumurugan7 <thiru82003@gmail.com>
2023-01-13 23:38:02 +0530

    mobile view of login page completed

--- frontend/src/components/inputs/loginInput/index.js
+++ frontend/src/components/inputs/loginInput/index.js
@@ -5,7 +5,7 @@ export default function LoginInput({ placeholder, bottom, ...props }) {
   return (
     <div className="input_wrap">
       {meta.touched && meta.error && !bottom && (
-        <div className="input_error">
+        <div className="input_error" style={{ transform: "translateY(3px)" }}>
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div className="error_arrow_top"></div>
@@ -22,10 +22,10 @@ export default function LoginInput({ placeholder, bottom, ...props }) {
       />
 
       {meta.touched && meta.error && bottom && (
-        <div className="input_error">
+        <div className="input_error" style={{ transform: "translateY(2px)" }}>
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className="error_arrow_top"></div>
+            <div className="error_arrow_bottom"></div>
           )}
         </div>
       )}
