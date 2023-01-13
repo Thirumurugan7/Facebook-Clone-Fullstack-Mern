73fb3bfe1058a104b82eb1447dc07e1251c28f1d
thirumurugan7 <thiru82003@gmail.com>
2023-01-13 22:55:46 +0530

    error messages on form

--- frontend/src/components/inputs/loginInput/index.js
+++ frontend/src/components/inputs/loginInput/index.js
@@ -4,11 +4,11 @@ export default function LoginInput({ placeholder, ...props }) {
   const [field, meta] = useField(props);
   return (
     <div className="input_wrap">
-      <div>
+      <div className="input_error">
         {meta.touched && meta.error && <ErrorMessage name={field.name} />}
       </div>
       <input
-        className={meta.touched && meta.error && "input_error_border"}
+        className={meta.touched && meta.error ? "input_error_border" : ""}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
