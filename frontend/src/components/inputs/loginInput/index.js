650fad0464b3d9e74c3f2030128c063a6594ba5d
thirumurugan7 <thiru82003@gmail.com>
2023-01-13 23:01:27 +0530

    changed all the icons

--- frontend/src/components/inputs/loginInput/index.js
+++ frontend/src/components/inputs/loginInput/index.js
@@ -4,9 +4,11 @@ export default function LoginInput({ placeholder, ...props }) {
   const [field, meta] = useField(props);
   return (
     <div className="input_wrap">
-      <div className="input_error">
-        {meta.touched && meta.error && <ErrorMessage name={field.name} />}
-      </div>
+      {meta.touched && meta.error && (
+        <div className="input_error">
+          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
+        </div>
+      )}
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
         type={field.type}
