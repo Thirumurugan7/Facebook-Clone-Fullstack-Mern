8359ab97a0d5373ef07977432182254a4274a629
thirumurugan7 <thiru82003@gmail.com>
2023-01-13 22:37:29 +0530

    using useField

--- frontend/src/components/inputs/loginInput/index.js
+++ frontend/src/components/inputs/loginInput/index.js
@@ -1,9 +1,16 @@
 import "./style.css";
-
-export default function LoginInput() {
+import { Field, useField } from "formik";
+export default function LoginInput({ placeholder, ...props }) {
+  const [field, meta] = useField(props);
   return (
     <div className="input_wrap">
-      <input type="text" />
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
     </div>
   );
 }
