1647cd31a948e6ec3a1c708273f9722475078334
thirumurugan7 <thiru82003@gmail.com>
2023-01-14 09:33:20 +0530

    register form building

--- frontend/src/components/login/Register.js
+++ frontend/src/components/login/Register.js
@@ -1,4 +1,23 @@
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import RegisterInput from "../inputs/registerInput";
+const userInfos = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+  bYear: "",
+  bMonth: "",
+  bDay: "",
+  gender: "",
+};
 export default function Register() {
+  const [user, setUser] = useState(userInfos);
+
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
   return (
     <div className="blur">
       <div className="register">
@@ -7,6 +26,20 @@ export default function Register() {
           <span>Sign Up</span>
           <span>It's quick and easy</span>
         </div>
+        <Formik>
+          {(formik) => {
+            <Form className="register_form">
+              <div className="regi_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="First Name"
+                  name="first_name"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+            </Form>;
+          }}
+        </Formik>
       </div>
     </div>
   );
