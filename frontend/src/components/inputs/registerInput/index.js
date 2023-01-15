27ad7353a3ca6e4f5fd700364af6701ce36f0d3a
thirumurugan7 <thiru82003@gmail.com>
2023-01-15 12:12:39 +0530

    changing register lbesl

--- frontend/src/components/inputs/registerInput/index.js
+++ frontend/src/components/inputs/registerInput/index.js
@@ -8,21 +8,6 @@ export default function RegisterInput({ placeholder, bottom, ...props }) {
   });
   return (
     <div className="input_wrap">
-      {meta.touched && meta.error && !bottom && (
-        <div
-          className={
-            desktopView ? "input_error input_error_desktop" : "input_error"
-          }
-          style={{ transform: "translateY(3px)" }}
-        >
-          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
-          {meta.touched && meta.error && (
-            <div
-              className={desktopView ? "error_arrow_left" : "error_arrow_top"}
-            ></div>
-          )}
-        </div>
-      )}
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
         type={field.type}
