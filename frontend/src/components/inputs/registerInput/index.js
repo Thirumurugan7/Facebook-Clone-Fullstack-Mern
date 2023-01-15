11f6eab3c57263fd510bcd0f3b11c5c48ea06064
thirumurugan7 <thiru82003@gmail.com>
2023-01-15 12:28:32 +0530

    using mediaqueries

--- frontend/src/components/inputs/registerInput/index.js
+++ frontend/src/components/inputs/registerInput/index.js
@@ -3,13 +3,29 @@ import { useField, ErrorMessage } from "formik";
 import { useMediaQuery } from "react-responsive";
 export default function RegisterInput({ placeholder, bottom, ...props }) {
   const [field, meta] = useField(props);
-  const desktopView = useMediaQuery({
+  const view1 = useMediaQuery({
+    query: "(min-width:539px)",
+  });
+  const view2 = useMediaQuery({
     query: "(min-width: 850px)",
+  });
+
+  const view3 = useMediaQuery({
+    query: "(min-width:1170)",
   });
   return (
     <div className="input_wrap register_input_wrap">
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
+        style={{
+          width: `${
+            (view1 && field.name === "first_name") || field.name === "last_name"
+              ? "100%"
+              : view1 && (field.name === "email" || field.name === "password")
+              ? "370px"
+              : "300px"
+          }`,
+        }}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
