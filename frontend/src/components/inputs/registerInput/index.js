2ce8aea4f2e036cffcd54f15f7e375813e598a55
thirumurugan7 <thiru82003@gmail.com>
2023-01-15 12:50:23 +0530

    working on responsiveness

--- frontend/src/components/inputs/registerInput/index.js
+++ frontend/src/components/inputs/registerInput/index.js
@@ -11,8 +11,11 @@ export default function RegisterInput({ placeholder, bottom, ...props }) {
   });
 
   const view3 = useMediaQuery({
-    query: "(min-width:1170)",
+    query: "(min-width:1170px)",
   });
+
+  const test1 = view3 && field.name === "first_name";
+  const test2 = view3 && field.name === "last_name";
   return (
     <div className="input_wrap register_input_wrap">
       <input
@@ -32,30 +35,29 @@ export default function RegisterInput({ placeholder, bottom, ...props }) {
         {...field}
         {...props}
       />
-      {meta.touched && meta.error && bottom && (
+      {meta.touched && meta.error && (
         <div
-          className={
-            desktopView ? "input_error input_error_desktop" : "input_error"
-          }
-          style={{ transform: "translateY(2px)" }}
+          className={view3 ? "input_error input_error_desktop" : "input_error"}
+          style={{
+            transform: "translateY(2px)",
+            left: `${test1 ? "-107%" : test2 ? "107%" : ""}`,
+          }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div
               className={
-                desktopView ? "error_arrow_left" : "error_arrow_bottom"
+                view3 && field.name !== "last_name"
+                  ? "error_arrow_left"
+                  : view3 && field.name === "last_name"
+                  ? "error_arrow_right"
+                  : !view3 && "error_arrow_bottom"
               }
             ></div>
           )}
         </div>
       )}
 
-      {meta.touched && meta.error && (
-        <i
-          className="error_icon"
-          style={{ top: `${!bottom && !desktopView ? "63%" : "15px"}` }}
-        ></i>
-      )}
       {meta.touched && meta.error && <i className="error_icon"></i>}
     </div>
   );
