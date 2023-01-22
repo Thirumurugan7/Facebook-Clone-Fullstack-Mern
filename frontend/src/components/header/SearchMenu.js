88555ba4ca3c60992428372d697328d606c0f9c0
thirumurugan7 <thiru82003@gmail.com>
2023-01-22 11:16:30 +0530

    working on icons

--- frontend/src/components/header/SearchMenu.js
+++ frontend/src/components/header/SearchMenu.js
@@ -1,19 +1,48 @@
+import { useRef, useState } from "react";
 import { Return, Search } from "../../svg";
 
 export default function SearchMenu({ color }) {
+  const [iconVisible, setIconVisible] = useState(true);
+  const menu = useRef(null);
+  const input = useRef(null);
+  useClickOutside(menu, () => {
+    setShowSearch(false);
+  });
   return (
     <div className="header_left search_area scrollbar">
       <div className="search_wrap">
         <div className="header_logo">
-          <div className="circle hover1">
+          <div
+            className="circle hover1"
+            onClick={() => {
+              setShowSearchMenu(false);
+            }}
+          >
             <Return color={color} />
           </div>
         </div>
-        <div className="search">
-          <div>
-            <Search color={color} />
-          </div>
-          <input type="text" placeholder="Search Facebook" />
+        <div
+          className="search"
+          onClick={() => {
+            input.current.focus();
+          }}
+        >
+          {iconVisible && (
+            <div>
+              <Search color={color} />
+            </div>
+          )}
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            ref={input}
+            onFocus={() => {
+              setIconVisible(false);
+            }}
+            onBlur={() => {
+              setIconVisible(true);
+            }}
+          />
         </div>
       </div>
       <div className="search_history_header">
